017863d4c724fb5ae54c54afb85f82698a87a3ea
Pratham Jaiswal <52135403+PR47H4M@users.noreply.github.com>
2019-10-04 23:37:07 +0530

    main

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login(){
     var username = document.getElementById("uname").value;
     var password = document.getElementById("password").value;
     if(username=="Pratham" && password=="pr47h4m"){
-        window.open("https://prathamjaiswal.github.io/CodeTalks/index.html"/*,"_self","_parent"*/);
+        window.open("https://prathamjaiswal.github.io/CodeTalks/home.html"/*,"_self","_parent"*/);
 
     }else{
         window.alert("Invalid Username or Password");
